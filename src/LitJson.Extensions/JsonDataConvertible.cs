9ec6b21da659826ad7a680863eecd0909b2c3117
JLChnToZ <jeremylam619@gmail.com>
2016-07-24 10:09:51 +0800

    Make JsonData not depends on System.Convert class and will handle all conversion cases.

--- src/LitJson.Extensions/JsonDataConvertible.cs
+++ src/LitJson.Extensions/JsonDataConvertible.cs
@@ -15,156 +15,197 @@ namespace LitJson {
             }
         }
 
-        object GetPrimitive() {
-            switch(type) {
-                case JsonType.Boolean: return inst_boolean;
-                case JsonType.Int: return inst_int;
-                case JsonType.Long: return inst_long;
-                case JsonType.Double: return inst_double;
-                case JsonType.String: return inst_string;
-                default: return null;
-            }
-        }
-
         bool IConvertible.ToBoolean(IFormatProvider provider) {
             switch(type) {
+                case JsonType.None: return false;
                 case JsonType.Boolean: return inst_boolean;
                 case JsonType.Int: return inst_int != 0;
                 case JsonType.Long: return inst_long != 0;
                 case JsonType.Double: return inst_double != 0;
+                case JsonType.String: return bool.Parse(inst_string);
+                default: throw new InvalidCastException();
             }
-            return Convert.ToBoolean(GetPrimitive(), provider);
         }
 
         sbyte IConvertible.ToSByte(IFormatProvider provider) {
             switch(type) {
+                case JsonType.None: return 0;
                 case JsonType.Int: return (sbyte)inst_int;
                 case JsonType.Long: return (sbyte)inst_long;
                 case JsonType.Double: return (sbyte)inst_double;
                 case JsonType.Boolean: return inst_boolean ? (sbyte)1 : (sbyte)0;
+                case JsonType.String: return sbyte.Parse(inst_string, provider);
+                default: throw new InvalidCastException();
             }
-            return Convert.ToSByte(GetPrimitive(), provider);
         }
 
         short IConvertible.ToInt16(IFormatProvider provider) {
             switch(type) {
+                case JsonType.None: return 0;
                 case JsonType.Int: return (short)inst_int;
                 case JsonType.Long: return (short)inst_long;
                 case JsonType.Double: return (short)inst_double;
                 case JsonType.Boolean: return inst_boolean ? (short)1 : (short)0;
+                case JsonType.String: return short.Parse(inst_string, provider);
+                default: throw new InvalidCastException();
             }
-            return Convert.ToInt16(GetPrimitive(), provider);
         }
 
         int IConvertible.ToInt32(IFormatProvider provider) {
             switch(type) {
+                case JsonType.None: return 0;
                 case JsonType.Int: return inst_int;
                 case JsonType.Long: return (int)inst_long;
                 case JsonType.Double: return (int)inst_double;
                 case JsonType.Boolean: return inst_boolean ? 1 : 0;
+                case JsonType.String: return int.Parse(inst_string, provider);
+                default: throw new InvalidCastException();
             }
-            return Convert.ToInt32(GetPrimitive(), provider);
         }
 
         long IConvertible.ToInt64(IFormatProvider provider) {
             switch(type) {
+                case JsonType.None: return 0L;
                 case JsonType.Long: return inst_long;
                 case JsonType.Int: return inst_int;
                 case JsonType.Double: return (long)inst_double;
                 case JsonType.Boolean: return inst_boolean ? 1L : 0L;
+                case JsonType.String: return long.Parse(inst_string, provider);
+                default: throw new InvalidCastException();
             }
-            return Convert.ToInt64(GetPrimitive(), provider);
         }
 
         byte IConvertible.ToByte(IFormatProvider provider) {
             switch(type) {
+                case JsonType.None: return 0;
                 case JsonType.Int: return (byte)inst_int;
                 case JsonType.Long: return (byte)inst_long;
                 case JsonType.Double: return (byte)inst_double;
                 case JsonType.Boolean: return inst_boolean ? (byte)1 : (byte)0;
+                case JsonType.String: return byte.Parse(inst_string, provider);
+                default: throw new InvalidCastException();
             }
-            return Convert.ToByte(GetPrimitive(), provider);
         }
 
         ushort IConvertible.ToUInt16(IFormatProvider provider) {
             switch(type) {
+                case JsonType.None: return 0;
                 case JsonType.Int: return (ushort)inst_int;
                 case JsonType.Long: return (ushort)inst_long;
                 case JsonType.Double: return (ushort)inst_double;
                 case JsonType.Boolean: return inst_boolean ? (ushort)1 : (ushort)0;
+                case JsonType.String: return ushort.Parse(inst_string, provider);
+                default: throw new InvalidCastException();
             }
-            return Convert.ToUInt16(GetPrimitive(), provider);
         }
 
         uint IConvertible.ToUInt32(IFormatProvider provider) {
             switch(type) {
+                case JsonType.None: return 0U;
                 case JsonType.Int: return (uint)inst_int;
                 case JsonType.Long: return (uint)inst_long;
                 case JsonType.Double: return (uint)inst_double;
                 case JsonType.Boolean: return inst_boolean ? 1U : 0U;
+                case JsonType.String: return uint.Parse(inst_string, provider);
+                default: throw new InvalidCastException();
             }
-            return Convert.ToUInt32(GetPrimitive(), provider);
         }
 
         ulong IConvertible.ToUInt64(IFormatProvider provider) {
             switch(type) {
+                case JsonType.None: return 0UL;
                 case JsonType.Long: return (ulong)inst_long;
                 case JsonType.Int: return (ulong)inst_int;
                 case JsonType.Double: return (ulong)inst_double;
                 case JsonType.Boolean: return inst_boolean ? 1UL : 0UL;
+                case JsonType.String: return ulong.Parse(inst_string, provider);
+                default: throw new InvalidCastException();
             }
-            return Convert.ToUInt64(GetPrimitive(), provider);
         }
 
         float IConvertible.ToSingle(IFormatProvider provider) {
             switch(type) {
+                case JsonType.None: return 0F;
                 case JsonType.Double: return (float)inst_double;
                 case JsonType.Int: return inst_int;
                 case JsonType.Long: return inst_long;
                 case JsonType.Boolean: return inst_boolean ? 1F : 0F;
+                case JsonType.String: return float.Parse(inst_string, provider);
+                default: throw new InvalidCastException();
             }
-            return Convert.ToSingle(GetPrimitive(), provider);
         }
 
         double IConvertible.ToDouble(IFormatProvider provider) {
             switch(type) {
+                case JsonType.None: return 0D;
                 case JsonType.Double: return inst_double;
                 case JsonType.Int: return inst_int;
                 case JsonType.Long: return inst_long;
                 case JsonType.Boolean: return inst_boolean ? 1D : 0D;
+                case JsonType.String: return double.Parse(inst_string, provider);
+                default: throw new InvalidCastException();
             }
-            return Convert.ToDouble(GetPrimitive(), provider);
         }
 
         decimal IConvertible.ToDecimal(IFormatProvider provider) {
             switch(type) {
+                case JsonType.None: return 0M;
                 case JsonType.Int: return inst_int;
                 case JsonType.Long: return inst_long;
                 case JsonType.Double: return (decimal)inst_double;
                 case JsonType.Boolean: return inst_boolean ? 1M : 0M;
+                case JsonType.String: return decimal.Parse(inst_string, provider);
+                default: throw new InvalidCastException();
             }
-            return Convert.ToDecimal(GetPrimitive(), provider);
         }
 
         char IConvertible.ToChar(IFormatProvider provider) {
-            return Convert.ToChar(GetPrimitive(), provider);
+            switch(type) {
+                case JsonType.None: return '\0';
+                case JsonType.Int: return (char)inst_int;
+                case JsonType.Long: return (char)inst_long;
+                case JsonType.Double: return (char)inst_double;
+                case JsonType.String: return char.Parse(inst_string);
+                default: throw new InvalidCastException();
+            }
         }
 
         string IConvertible.ToString(IFormatProvider provider) {
-            if(type == JsonType.String) return inst_string;
-            return Convert.ToString(GetPrimitive(), provider);
+            switch(type) {
+                case JsonType.None: return string.Empty;
+                case JsonType.Int: return inst_int.ToString(provider);
+                case JsonType.Long: return inst_long.ToString(provider);
+                case JsonType.Double: return inst_double.ToString(provider);
+                case JsonType.Boolean: return inst_boolean.ToString(provider);
+                case JsonType.String: return inst_string;
+                case JsonType.Array: return string.Format("[JSON Array ({0})]", EnsureCollection().Count);
+                case JsonType.Object: return string.Format("[JSON Object ({0})]", EnsureCollection().Count);
+                default: return ToString();
+            }
         }
 
         DateTime IConvertible.ToDateTime(IFormatProvider provider) {
-            return Convert.ToDateTime(GetPrimitive(), provider);
+            switch(type) {
+                case JsonType.String: return DateTime.Parse(inst_string, provider);
+                default: throw new InvalidCastException();
+            }
         }
 
         object IConvertible.ToType(Type conversionType, IFormatProvider provider) {
-            if(conversionType == typeof(JsonData))
+            if(conversionType == null)
+                throw new ArgumentNullException("conversionType");
+            if(conversionType.IsAssignableFrom(typeof(JsonData)))
                 return this;
-            IConvertible rawValue = GetPrimitive() as IConvertible;
-            if(rawValue == this)
-                throw new NotSupportedException();
+            IConvertible rawValue;
+            switch(type) {
+                case JsonType.None: return null;
+                case JsonType.Boolean: rawValue = inst_boolean; break;
+                case JsonType.Int: rawValue = inst_int; break;
+                case JsonType.Long: rawValue = inst_long; break;
+                case JsonType.Double: rawValue = inst_double; break;
+                case JsonType.String: rawValue = inst_string; break;
+                default: throw new InvalidCastException();
+            }
             return rawValue.ToType(conversionType, provider);
         }
     }
