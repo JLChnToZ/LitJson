df8cca403dbb61dec04732bf357cd319f35f50ea
JLChnToZ <jeremylam619@gmail.com>
2016-10-02 23:40:25 +0800

    Add enumerator (with auto null check) extensions

--- src/LitJson.Extensions/Extensions.cs
+++ src/LitJson.Extensions/Extensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using LitJson.ExtensionsHelpers;
 
 namespace LitJson {
     public static class Extensions {
@@ -319,45 +320,14 @@ namespace LitJson {
             return child;
         }
 
-        private class PlaceHolder {
-            public readonly IJsonWrapper source;
-            private readonly ArrayList arrayList;
-            private readonly Hashtable hashtable;
-            private readonly int index;
-            private readonly object key;
-
-            public PlaceHolder(IJsonWrapper source) {
-                this.source = source;
-                this.arrayList = null;
-                this.index = 0;
-                this.hashtable = null;
-                this.key = null;
-            }
-
-            public PlaceHolder(IJsonWrapper source, ArrayList arrayList, int index) {
-                this.source = source;
-                this.arrayList = arrayList;
-                this.index = index;
-                this.hashtable = null;
-                this.key = null;
-            }
-
-            public PlaceHolder(IJsonWrapper source, Hashtable hashtable, object key) {
-                this.source = source;
-                this.arrayList = null;
-                this.index = 0;
-                this.hashtable = hashtable;
-                this.key = key;
-            }
-
-            public void Assign(object value) {
-                if(arrayList != null)
-                    arrayList[index] = value;
-                else if(hashtable != null)
-                    hashtable[key] = value;
-            }
+        public static IEnumerable<IJsonWrapper> GetChilds(this IJsonWrapper source) {
+            return new JsonWrapperEnumerable(source);
         }
-    
+
+        public static IEnumerable<KeyValuePair<string, IJsonWrapper>> GetChildsKeyValuePair(this IJsonWrapper source) {
+            return new JsonWrapperKeyValueEnumerable(source);
+        }
+
         public static object Box(this IJsonWrapper source) {
             if(source == null) return null;
             var checkList = new List<PlaceHolder> { new PlaceHolder(source) };
